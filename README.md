1fd0b982a74d4d4d0858227114960e96ecc466e0
bobbs04 <win3ldestiny@gmail.com>
2023-03-19 00:40:27 +0300

    string concatenation project

---
+++ README.md
@@ -0,0 +1,3 @@
+Python Basics
+Day 2 - 3
+strings, integers, floats, lists, tuples, sets and dictionary
---
+++ madlip.py
@@ -0,0 +1,23 @@
+# string/str concatenation
+# mad lip project
+print('add the missing  word in the gap')
+adj1 = input('adj1: ')
+adj2 = input('adj2: ')
+adj3 = input('adj3: ')
+adj4 = input('adj4: ')
+adj5 = input('adj5: ')
+adj6 = input('adj6: ')
+adj7 = input('adj7: ')
+adj8 = input('adj8: ')
+adj9 = input('adj9: ')
+
+print('Worthy Love')
+story = f'One day a husband {adj1} home a bouquet of {adj2} \
+roses for his wife. The wife was pleasantly {adj3} and {adj4}, \
+but before she could ask what they were for, their daughter {adj5} \
+up, "What are they for, Daddy?" \
+He {adj6} and said "All my colleagues were discussing how {adj7} \
+they were of their marriage.It made me feel so lucky to have your Mommy \
+in my life. She is the most {adj8} and {adj9} woman in the entire world!'
+
+print(story)
